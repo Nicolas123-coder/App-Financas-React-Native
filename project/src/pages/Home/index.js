37e1bb2c029e863f27403ec068d6b16b61818985
Nicolas de Barros <nicolasdbarros@yahoo.com.br>
2023-02-13 17:51:59 -0300

    Creating Balance Component

--- project/src/pages/Home/index.js
+++ project/src/pages/Home/index.js
@@ -1,11 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from '../../components/Header';
+import Balance from '../../components/Balance';
+
+const list = [
+  {
+    id: 1,
+    label: 'Boleto Luz',
+    value: '300,50',
+    date: '17/09/2023',
+    type: 0 //despesas
+  },
+  {
+    id: 2,
+    label: 'Pix Cliente',
+    value: '1900,50',
+    date: '20/09/2023',
+    type: 1 //despesas
+  },
+  {
+    id: 3,
+    label: 'Pix Cliente 2',
+    value: '1900,50',
+    date: '20/09/2023',
+    type: 1 //despesas
+  }
+]
 
 export default function Home() {
   return (
     <View style={styles.container}>
         <Header name='Nicolas de Barros'/>
-      <Text>Open up App.js to start working on your app!</Text>
+        <Balance saldo='10.00.00' gastos='-500,00'/>
+
+        <Text style={styles.title}>Últimas Movimentações</Text>
+        <FlatList 
+          style={styles.list}
+          data={list}
+          keyExtractor={ (item) => String(item.id) }
+          showsVerticalScrollIndicator = {false}
+          renderItem={ ({ item }) => <Text>TESTE</Text> }
+        />
+
     </View>
   );
 }
@@ -13,6 +48,18 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fafafa',
   },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginLeft: 14,
+    marginRight:14,
+    marginTop: 14
+  },
+  list: {
+    marginStart: 14,
+    marginEnd: 14,
+
+  }
 });
